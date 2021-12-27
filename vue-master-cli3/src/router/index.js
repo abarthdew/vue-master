298a612b68988049018e5b927ce7971d43c2ff09
ABarthDew <vsevolod117@gmail.com>
2021-12-27 22:54:51 +0900

    02_router_import_views_pages_and_set_routes_component

--- vue-master-cli3/src/router/index.js
+++ vue-master-cli3/src/router/index.js
@@ -1,21 +1,24 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router'; // get router
+import NewsView from '../views/NewsView';
+import AskView from '../views/AskView';
+import JobsView from '../views/JobsView';
 
 Vue.use(vueRouter); // vue router setting
 
 const router = new VueRouter({ // make new router Object
     routes: [ // set router information inside Object
         {
-            path: '/', // url address
-            component: '', // displayed component for url; aka 'page'
+            path: '/news', // url address
+            component: NewsView, // displayed component for url; aka 'page'
         },
         {
-            path: '',
-            component: '',
+            path: '/ask', // If you go to '/ask',
+            component: AskView, // You can see this page
         },
         {
-            path: '',
-            component: '',
+            path: '.jobs',
+            component: JobsView,
         }
     ],
 })
