3d8184fbc41da03dacfd8b57a57b537194b18104
ABarthDew <vsevolod117@gmail.com>
2021-12-28 21:36:57 +0900

    05_router_link_css

--- vue-master-cli3/src/App.vue
+++ vue-master-cli3/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <too-bar></too-bar>
     <router-view></router-view>
   </div>
@@ -26,6 +25,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
