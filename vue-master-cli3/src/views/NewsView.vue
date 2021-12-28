af66df812b400190e45b3c0666e01828268a5d6d
ABarthDew <vsevolod117@gmail.com>
2021-12-28 22:16:24 +0900

    06_axios

--- vue-master-cli3/src/views/NewsView.vue
+++ vue-master-cli3/src/views/NewsView.vue
@@ -1,3 +1,34 @@
 <template>
-    <div>news-view</div>
+    <div>
+        <li v-for="user in users" :key="user">
+            {{ user.title }}
+        </li>
+    </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            users: [],
+        }
+    },
+    created() {
+        axios.get('https://api.hnpwa.com/v0/news/1.json')
+        .then(res => {
+            console.log(res.data)
+            this.users = res.data;
+        })
+        .catch(error => console.log(error));
+// ==>  // new Promise()
+        // .then(function(response) {
+        //     console.log(response);
+        // })
+        // .catch(function() {
+
+        // })
+    }
+}
+</script>
