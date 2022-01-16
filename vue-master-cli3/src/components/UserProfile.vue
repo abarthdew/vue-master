bba3f92ce2f6674cdd4d40fb91b646de7c66d2e2
ABarthDew <vsevolod117@gmail.com>
2022-01-16 17:08:30 +0900

    24_user_profile_1

---
+++ vue-master-cli3/src/components/UserProfile.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <section>
+            <!-- question detail-->
+            <div style="margin:5px; padding:10px; background:#9acd32; width: 90px;">
+                [UserName]
+                <!-- <router-link :to="`/user/${fetchedItem.user}`">{{ fetchedItem.id }}</router-link> -->
+            </div>
+        </section>
+        <section>
+            <!-- question answer-->
+            <!-- <h3>{{ fetchedItem.title }}</h3> -->
+            <div v-html="fetchedItem.content" style="border:1px solid #ddd; padding: 5px;"></div>
+        </section>
+    </div>
+</template>
