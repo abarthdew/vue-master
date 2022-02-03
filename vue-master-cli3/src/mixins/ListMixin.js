2d7236b338886abf6c17c998d74edd217403541c
ABarthDew <vsevolod117@gmail.com>
2022-02-03 22:45:56 +0900

    38_router_spinner_2

--- vue-master-cli3/src/mixins/ListMixin.js
+++ vue-master-cli3/src/mixins/ListMixin.js
@@ -2,6 +2,7 @@ import bus from '../utils/bus'
 
 export default {
     // reusable component option
-    created() { 
-    },
+    mounted() {
+        bus.$emit('end:spinner');
+    }
 }
--- vue-master-cli3/src/views/JobsView.vue
+++ vue-master-cli3/src/views/JobsView.vue
@@ -3,15 +3,13 @@
 </template>
 
 <script>
+import ListMixin from '../mixins/ListMixin'
 import ListItem from '../components/ListItem.vue'
-import bus from '../utils/bus'
 
 export default {
     components: {
         ListItem,
     },
-    mounted() {
-        bus.$emit('end:spinner');
-    }
+    mixins: [ListMixin]
 }
 </script>
