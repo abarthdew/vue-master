80d671aafdc704ae63296cd050fa307a557164ee
ABarthDew <vsevolod117@gmail.com>
2022-02-04 23:53:50 +0900

    39_async_await_1

---
+++ vue-master-cli3/src/views/AsyncAwait.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <button @click="loginUser">login</button>
+        <h1>List</h1>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    methods: {
+        loginUser() {
+            axios.get('https://jsonplaceholder.typicode.com/users/1')
+            .then(response => {
+                if (response.data.id === 1 ) {
+                    axios.get('https://jsonplaceholder.typicode.com/todos')
+                    .then(response2 => console.log(response2))
+                    .catch(error => console.log(error));
+                }
+            })
+            .catch(error => console.log(error));
+        }
+    }    
+}
+</script>
